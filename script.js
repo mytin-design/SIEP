91a933e6e29a2bb8371e08acc2cfb1d43960597f
mytin-design <denismytin@gmail.com>
2023-06-12 22:23:12 +0300

    sieinitial

---
+++ script.js
@@ -0,0 +1,7 @@
+function openGate() {
+    document.getElementById("sideNavigation").style.width = "15pc";
+}
+
+function closeGate() {
+    document.getElementById("sideNavigation").style.width = "0";
+}
